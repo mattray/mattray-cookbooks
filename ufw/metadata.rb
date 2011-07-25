921f239ef349917335906abfc0d82750fb72d70c
mattray <matthewhray@gmail.com>
2011-07-25 16:50:47 -0500

    added firewall dependency

--- ufw/metadata.rb
+++ ufw/metadata.rb
@@ -2,8 +2,9 @@ maintainer       "Opscode, Inc."
 maintainer_email "matt@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures ufw"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.me'))
-version          "0.0.1"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1"
+depends          "firewall"
 
 %w{ ubuntu }.each do |os|
   supports os
