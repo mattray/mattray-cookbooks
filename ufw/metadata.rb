6bd934a0de1129100dc1bf236e89ffb020230e95
mattray <matthewhray@gmail.com>
2011-08-30 15:51:30 -0500

    supports logging attribute, requires firewall 0.6

--- ufw/metadata.rb
+++ ufw/metadata.rb
@@ -3,8 +3,8 @@ maintainer_email "matt@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures ufw"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.2"
-depends          "firewall", ">= 0.5.7"
+version          "0.5"
+depends          "firewall", ">= 0.6"
 
 %w{ ubuntu }.each do |os|
   supports os
